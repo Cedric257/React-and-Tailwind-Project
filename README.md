0992e7321e7dc99ddaedf036f5adc310759954a9
Cedric257 <cedricakimana257@gmail.com>
2025-05-03 12:38:27 +0200

    configuration

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# React + TypeScript + Vite
+# React + TypeScript + Vite + Tailwind CSS
+
+Dev: 
+# Cedric 
+```js 
+FrontEnd Web {
+  # Angular + React
+}
+```
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
   <div className='gap-60 flex flex-col'>
 <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 rounded mt-1.5">Submit</button>
 <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3 ms-1.5">Submit</button>
+
 </div>
     </>
   )
